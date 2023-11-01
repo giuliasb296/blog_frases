09d59362000525c067224de4d87f1bee38b71284
giuliasb296 <giuliasb296@gmail.com>
2023-11-01 16:45:54 -0300

    Atualização da Home

--- blog/src/app/frases/frases.component.ts
+++ blog/src/app/frases/frases.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class FrasesComponent {
   listaDeFrasesFiltradas: Frases[] = [];
   listaDeFrases = [
-    {frase: 'O amor não se vê com os olhos, mas com o coração. William Shakespeare', gender: 'Amor'},
-    {frase: 'O amor é o espaço e o tempo tornados sensíveis ao coração. Marcel Proust', gender: 'Amor'},
-    {frase: 'Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos. Friedrich Nietzsche', gender: 'Motivacionais'},
-    {frase: 'Comece fazendo o que é necessário, depois o que é possível, e de repente você estará fazendo o impossível.São  Francisco de Assis', gender: 'Motivacionais'},
-    {frase: 'Ser feliz sem motivo é a mais autêntica forma de felicidade. Carlos Drummond de Andrade', gender: 'Felicidade'},
-    {frase: 'Saber encontrar a alegria na alegria dos outros é o segredo da felicidade. Georges Bernanos', gender: 'Felicidade'}
+    {frase: '"O amor não se vê com os olhos, mas com o coração." - William Shakespeare', gender: 'Amor'},
+    {frase: '"O amor é o espaço e o tempo tornados sensíveis ao coração." - Marcel Proust', gender: 'Amor'},
+    {frase: '"Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos" - Friedrich Nietzsche', gender: 'Motivacionais'},
+    {frase: '"Comece fazendo o que é necessário, depois o que é possível, e de repente você estará fazendo o impossível." - São  Francisco de Assis', gender: 'Motivacionais'},
+    {frase: '"Ser feliz sem motivo é a mais autêntica forma de felicidade." - Carlos Drummond de Andrade', gender: 'Felicidade'},
+    {frase: '"Saber encontrar a alegria na alegria dos outros é o segredo da felicidade." - Georges Bernanos', gender: 'Felicidade'}
   ]
   filtrarFrases(value: string) {
     this.listaDeFrasesFiltradas = this.listaDeFrases.filter(frases => frases.gender === value);
